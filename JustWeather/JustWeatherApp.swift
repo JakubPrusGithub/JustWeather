35e14b96d29b27edc6bd1ca1f08502327643eef2
JakubPrusGithub <bluerbluerbluer@gmail.com>
2023-04-17 21:01:22 +0200

    Initial commit

---
+++ JustWeather/JustWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWeatherApp.swift
+//  JustWeather
+//
+//  Created by Jakub Prus on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct JustWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# JustWeather
+ This extravagant app shows the current weather in your area
